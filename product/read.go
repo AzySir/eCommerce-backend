cb61843a5ee445682a73d84c0335ccb95f2e3af9
Adam Sir <azy.sir@gmail.com>
2021-03-25 01:09:39 +0000

    Created function for GetAllProducts()

--- product/read.go
+++ product/read.go
@@ -1,11 +1,12 @@
 package product
 
 import (
-	"fmt"
+	// "fmt"
 	"log"
 	"database/sql"
 	_ "github.com/go-sql-driver/mysql" 
-	// "reflect"
+	"reflect"
+	"encoding/json"
 )
 
 var Connection *sql.DB;
@@ -29,14 +30,9 @@ type Product struct {
 
 func GetProduct(id int) Product {
 	var product Product
-	// conn, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", db_user, db_password, db_host, db_port, db))
-	conn, err := sql.Open("mysql", "root:admin@tcp(127.0.0.1:3306)/mydb")
-	if err != nil {
-		panic(err.Error()) // proper error handling instead of panic in your app
-	}
-	
+
 	// Execute the query
-	err = conn.QueryRow("SELECT * FROM product where id = ?", id).Scan(
+	err := Connection.QueryRow("SELECT * FROM product where id = ?", id).Scan(
 		&product.Id, 
 		&product.Name, 
 		&product.Brand, 
@@ -56,31 +52,30 @@ func GetProduct(id int) Product {
 		panic(err.Error()) // proper error handling instead of panic in your app
 	}
 
-	fmt.Println(product.Id)
-	fmt.Println(product.Name)
+	log.Println("GetProduct(): ")
+	log.Println(reflect.TypeOf(product))
 
 	return product
 }
 
-func GetAllProducts() {
-	conn, err := sql.Open("mysql", "root:admin@tcp(127.0.0.1:3306)/mydb")
-	ProductList := make([]*Product, 0)
-	fmt.Println("GetAllProducts")
+func GetAllProducts() []byte {
+	
+	//Declare Product List
+	var ProductList []*Product 
+
+	//Logging
+	log.Println("GetAllProducts() selected")
     
     // Execute the query
-    results, err := conn.Query("SELECT * FROM product")
+    results, err := Connection.Query("SELECT * FROM product")
 	if err != nil {
         log.Fatal(err)
     }
-	
-	fmt.Println("After First Error check")
 
-	fmt.Println("Loop starting..")
+	//Lopping through results and appending to ProductList
 	for results.Next() {
 		product := new(Product)
-
-		if err := results.Scan(
-				&product.Id, 
+		if err := results.Scan(&product.Id, 
 				&product.Name, 
 				&product.Brand, 
 				&product.Colour, 
@@ -96,17 +91,28 @@ func GetAllProducts() {
 		); err != nil {
 			panic(err)
 		}
+
+		//Needs to be appending as JSON...
 		ProductList = append(ProductList, product)
 	}
 
-	fmt.Println("After Loop")
+	p, _ := json.MarshalIndent(ProductList, "", "  ")
+	log.Println(string(p))
+
 	if err := results.Err(); err != nil {
 		panic(err)
 	}
 
-	for _, p := range ProductList {
-		fmt.Println(p.Name)
-	}
+
+	//Logging
+	//For Debugging: Loop through ProductList and print p.Name
+	log.Println("#############################")
+	log.Println(" ")
+	log.Println("   Result of Database Query  ")
+	log.Println(" ")
+	log.Println("#############################")
 
 	defer Connection.Close()
+
+	return p
 }
